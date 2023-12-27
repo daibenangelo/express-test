c0365c860d45a854c33f23deee21910297f460d0
progdaiben <daibenoatmeal3@gmail.com>
2023-12-27 09:43:59 +0800

    api setup

---
+++ pet_store.js
@@ -0,0 +1,39 @@
+const pets = [
+  {
+    id: 0,
+    species: "dog",
+    eating_habit: "omnivore",
+    pet_name: "Brownie",
+    image: "dog.jpg",
+  },
+  {
+    id: 1,
+    species: "cat",
+    eating_habit: "carnivore",
+    pet_name: "Luna",
+    image: "cat.webp",
+  },
+  {
+    id: 2,
+    species: "cat",
+    eating_habit: "carnivore",
+    pet_name: "Sunny",
+    image: "cat.jpg",
+  },
+  {
+    id: 3,
+    species: "parrot",
+    eating_habit: "omnivore",
+    pet_name: "Polly",
+    image: "parrot.jpg",
+  },
+  {
+    id: 4,
+    species: "goldfish",
+    eating_habit: "omnivore",
+    pet_name: "Goldy",
+    image: "goldfish.jpg",
+  },
+];
+
+module.exports = { pets };
